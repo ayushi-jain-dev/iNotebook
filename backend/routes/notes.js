b6df0855181142e715faa4ba4446385fa1fe2438
ayushi-jain-dev <jain.simmi1993@gmail.com>
2023-05-20 23:15:40 +0530

    Backend completed with all the end points

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,12 +1,119 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const Notes = require("../models/Notes");
+const Note = require("../models/Note");
+const { body, validationResult } = require("express-validator");
 var fetchUser = require("../middleware/fetchUser");
 
-//Route 1: Get all notes from database: GET "/api/auth/fetchallnotes". Login required
+//Route 1: Get all notes from database: GET "/api/notes/fetchallnotes". Login required
 
-router.get('/fetchallnotes', fetchUser, async (req,res)=>{
-    res.json([])
-})
+router.get("/fetchallnotes", fetchUser, async (req, res) => {
+    try {
+        const notes = await Note.find({ user: req.user.id });
 
-module.exports= router
+        res.json(notes);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+//Route 2: Add a new note using POST "/api/notes/addnote". Login required
+
+router.post(
+    "/addnote",
+    fetchUser,
+    [
+        body("title", "Enter a valid title").isLength({ min: 3 }),
+        body("description", "Description must be atleast 5 characters").isLength({
+            min: 5,
+        }),
+    ],
+    async (req, res) => {
+        try {
+            const { title, description, tag } = req.body;
+            //If there are errors, return Bad Request and the errors
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const note = new Note({
+                title,
+                description,
+                tag,
+                user: req.user.id,
+            });
+            const savedNote = await note.save();
+            res.json(savedNote);
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send("Internal Server Error");
+        }
+    }
+);
+//Route 3: Update an existing note using PUT"/api/notes/updatenote". Login required
+
+router.put("/updatenote/:id", fetchUser, async (req, res) => {
+    try {
+        const { title, description, tag } = req.body;
+        //Create a newNote Object
+
+        const newNote = {};
+        if (title) {
+            newNote.title = title;
+        }
+        if (description) {
+            newNote.description = description;
+        }
+        if (tag) {
+            newNote.tag = tag;
+        }
+
+        //find the note to be updated and update it
+
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            res.status(404).send("Not Found");
+        }
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+
+        note = await Note.findByIdAndUpdate(
+            req.params.id,
+            { $set: newNote },
+            { new: true }
+        );
+        res.json({ note });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+//Route 4: Delete an existing note using DELETE"/api/notes/deletenote". Login required
+
+router.delete("/deletenote/:id", fetchUser, async (req, res) => {
+    try {
+        //find the note to be delete and delete it
+
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            res.status(404).send("Not Found");
+        }
+
+        // Allow deletion only if user owns this note
+
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not Allowed");
+        }
+
+        note = await Note.findByIdAndDelete(req.params.id);
+        res.json({ Success: "Note has been deleted", note: note });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+module.exports = router;
